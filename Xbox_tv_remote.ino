da5f15e20641239a937c098fbd33b38d9b130a99
Piotr Smarzynski <smarzony@gmail.com>
2019-08-18 20:51:20 +0200

    Problemy z Prawym przyciskiem wynikają z zasilania. Są spadki napięcia, które powodują przechodzenie poniżej progu komparacji. Do rozwiązania elektrycznie poprzez fizyczne pullupy.

--- Xbox_tv_remote.ino
+++ Xbox_tv_remote.ino
@@ -8,8 +8,12 @@
 #include <avr/interrupt.h>
 
 #define BUTTON_LEFT_PIN 2
-#define BUTTON_RIGHT_PIN 4
+#define BUTTON_RIGHT_PIN 3
 #define IR_SEND_PIN 1
+
+#define STANDARD 0
+#define LEFT_ONLY 1
+#define RIGHT_ONLY 2
 
 IRsend irsend;
 
@@ -22,6 +26,7 @@ int last_button_state_left;
 bool last_pressed_state_left;
 long long hold_start_left, hold_stop_left;
 long long wake_up_time, actual_time;
+short mode;
 
 void setup()
 {
@@ -29,69 +34,89 @@ void setup()
 
 	// Setup button timers (all in milliseconds / ms)
 	// (These are default if not set, but changeable for convenience)
-	ButtonLeft.debounceTime = 20;   // Debounce timer in ms
+	ButtonLeft.debounceTime = 50;   // Debounce timer in ms
 	ButtonLeft.multiclickTime = 250;  // Time limit for multi clicks
 	ButtonLeft.longClickTime = 1000; // time until "held-down clicks" register
 
-	ButtonRight.debounceTime = 20;   // Debounce timer in ms
+	ButtonRight.debounceTime = 50;   // Debounce timer in ms
 	ButtonRight.multiclickTime = 250;  // Time limit for multi clicks
 	ButtonRight.longClickTime = 1000; // time until "held-down clicks" register
 
-
 	 // Serial.begin(9600);
+
+	mode = STANDARD;
 }
 
 
 void loop()
 {
-	actual_time = millis();
-	if (actual_time - wake_up_time > 10000)
-		sleep();
-
 	// Update button state
 	ButtonLeft.Update();
 	ButtonRight.Update();
 
 	/*
+	actual_time = millis();
+	if (actual_time - wake_up_time > 10000)
+		sleep();
+	
 	clickPrintLeft();
 	holdPrintLeft();
 	pressPrintLeft();
 	holdTimeLeft();
 	*/
 
-	if (ButtonLeft.clicks == 1 || ButtonLeft.clicks == -1)
+
+	if (mode == STANDARD)
 	{
-		//Serial.println("Sent Vol+ full");
-		irsend.sendRC5(0x10010, 12);
+		//------------------ LEFT BUTTON -------------------------
+		if (ButtonLeft.clicks == 1 || ButtonLeft.clicks == -1)
+		{
+			//Serial.println("Sent Vol- full");
+			irsend.sendRC5(0x10011, 12);
+		}
+
+		if (ButtonLeft.clicks == 2 || ButtonLeft.clicks == -2)
+		{
+			// Serial.println("Sent P-");
+			irsend.sendRC5(0x10021, 12);
+		}
+		if (ButtonLeft.clicks == 3 || ButtonLeft.clicks == -3)
+		{
+			//Serial.println("Sent POWER");
+			irsend.sendRC5(0x1000C, 12);
+		}
+		if (ButtonLeft.clicks == 4 || ButtonLeft.clicks == -4) // workaround if right button fails - there are voltage dropdowns 
+			
+		{
+			//Serial.println("Sent Vol+ full");
+			irsend.sendRC5(0x10010, 12);
+		}
+
+		//------------------ RIGHT BUTTON ------------------------
+		if (ButtonRight.clicks == 1 || ButtonRight.clicks == -1)
+		{
+			//Serial.println("Sent Vol+ full");
+			irsend.sendRC5(0x10010, 12);
+		}
+
+		if (ButtonRight.clicks == 2 || ButtonRight.clicks == 2)
+		{
+			//Serial.println("Sent P+ full");
+			irsend.sendRC5(0x10020, 12);
+		}
+
+
+		if (ButtonRight.clicks == 3 || ButtonRight.clicks == -3)
+		{
+			//Serial.println("Sent POWER");
+			irsend.sendRC5(0x1000C, 12);
+		}
 	}
 
-	if (ButtonLeft.clicks == 2 || ButtonLeft.clicks == -2)
-	{
-		//Serial.println("Sent P+ full");
-		irsend.sendRC5(0x10020, 12);
-	}
-
-	if (ButtonRight.clicks == 1 || ButtonRight.clicks == -1)
-	{
-		//Serial.println("Sent Vol- full");
-		irsend.sendRC5(0x10011, 12);
-	}
-
-	if (ButtonRight.clicks == 2 || ButtonRight.clicks == 2)
-	{
-		// Serial.println("Sent P-");
-		irsend.sendRC5(0x10021, 12);
-	}
-
-
-	if (ButtonRight.clicks == 3 || ButtonRight.clicks == -3)
-	{
-		//Serial.println("Sent POWER");
-		irsend.sendRC5(0x1000C, 12);
-	}
-
+	/*
 	last_button_state_left = ButtonLeft.clicks;
 	last_pressed_state_left = ButtonLeft.depressed;
+	*/
 }
 
 void sleep() {
